edd93aec699fc062936338bfc01394c0f69f9a96
Gurpreet <gurpreetsingh03.2001@gmail.com>
2024-06-12 11:45:24 +0530

    minnor changes

--- employee/src/main/java/com/manage/employee/entities/Designation.java
+++
@@ -1,6 +0,0 @@
- package com.manage.employee.entities;
-
-public enum Designation {
-	MANAGER,HR,SD1,SD2,SD,CONTRACT;
-
-}
--- employee/src/main/java/com/manage/employee/response/Response.java
+++
@@ -1,9 +0,0 @@
-package com.manage.employee.response;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.manage.employee.entities.Employee;
-
-public interface Response extends CrudRepository<Employee,Integer > {
-	public Employee findById(int id);
-}
